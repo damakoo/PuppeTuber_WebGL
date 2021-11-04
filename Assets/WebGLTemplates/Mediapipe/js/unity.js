c992b0cfd30ffbf411314d680cbea82fb8020263
damakoo <71433895+damakoo@users.noreply.github.com>
2021-11-04 15:35:11 +0900

    style調整

--- Assets/WebGLTemplates/Mediapipe/js/unity.js
+++ Assets/WebGLTemplates/Mediapipe/js/unity.js
@@ -2,16 +2,19 @@ const container_parent = document.getElementById('container');
 const container_child = document.getElementById('unity-container');
 
 let resize = () => {
-    if (container_parent.clientWidth <= container_parent.clientHeight * 1.6) {
+    if (container_parent.clientWidth <= container_parent.clientHeight * 1.65) {
         container_child.style.width = container_parent.clientWidth.toString() + 'px';
-        container_child.style.height = (container_parent.clientWidth / 1.6).toString() + 'px';
-    } else {
-        container_child.style.width = (container_parent.clientHeight * 1.6).toString() + 'px';
+        container_child.style.height = (container_parent.clientWidth / 1.65).toString() + 'px';
+    } else  if (container_parent.clientWidth >= container_parent.clientHeight * 1.55) {
+        container_child.style.width = (container_parent.clientHeight * 1.55).toString() + 'px';
         container_child.style.height = container_parent.clientHeight.toString() + 'px';
+    } else {
+        container_child.style.width = '100%';
+        container_child.style.height = '100%';
     }
 }
 let main = () => {
-    // window.addEventListener('load', resize, false);
+    window.addEventListener('load', resize, false);
     window.addEventListener('resize', resize, false);
 }
 main(); 
