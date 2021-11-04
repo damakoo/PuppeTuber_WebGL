d86241d0b341d59c4b5dfa5cd86e7fead8c14aff
damakoo <71433895+damakoo@users.noreply.github.com>
2021-11-04 18:39:43 +0900

    style調整

--- docs/js/unity.js
+++ docs/js/unity.js
@@ -2,11 +2,11 @@ const container_parent = document.getElementById('container');
 const container_child = document.getElementById('unity-container');
 
 let resize = () => {
-    if (container_parent.clientWidth <= container_parent.clientHeight * 1.65) {
+    if (container_parent.clientWidth <= container_parent.clientHeight * 1.55) {
         container_child.style.width = container_parent.clientWidth.toString() + 'px';
-        container_child.style.height = (container_parent.clientWidth / 1.65).toString() + 'px';
-    } else  if (container_parent.clientWidth >= container_parent.clientHeight * 1.55) {
-        container_child.style.width = (container_parent.clientHeight * 1.55).toString() + 'px';
+        container_child.style.height = (container_parent.clientWidth / 1.55).toString() + 'px';
+    } else  if (container_parent.clientWidth >= container_parent.clientHeight * 1.65) {
+        container_child.style.width = (container_parent.clientHeight * 1.65).toString() + 'px';
         container_child.style.height = container_parent.clientHeight.toString() + 'px';
     } else {
         container_child.style.width = '100%';
