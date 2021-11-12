f0b449826e6cebaa3044ab41ebddad03bc760faa
damakoo <71433895+damakoo@users.noreply.github.com>
2021-11-12 21:53:43 +0900

    make result UI

--- Assets/Aramaki/Scripts/ResultSceneManager.cs
+++ Assets/Aramaki/Scripts/ResultSceneManager.cs
@@ -4,15 +4,17 @@ using System.Collections;
 
 public class ResultSceneManager : MonoBehaviour
 {
+    ResultStep resultStep;
     [SerializeField] Text Synchronization_title;
     [SerializeField] Text Synchronization_value;
     [SerializeField] Text Instinct_title;
-    [SerializeField] Text Instinct_value;
-    [SerializeField] Text _name;
+    [SerializeField] Text Instinct_kyori;
+    [SerializeField] Text _shogo;
     [SerializeField] GameObject Unitychan_running;
     [SerializeField] GameObject UnityWaitBase;
     [SerializeField] GameObject UnityRunBase;
     private Animator runanimator;
+    Color color_dx;
     public static double result = 0;
     private int Number_Display = 0;
     private string name_title;
@@ -20,37 +22,86 @@ public class ResultSceneManager : MonoBehaviour
     private Vector3 destination;
     private int shogo_num;
     int intResult;
+    float unitychandx;
 
     // Start is called before the first frame update
     void Start()
     {
+        resultStep = ResultStep.Start;
         runanimator = Unitychan_running.GetComponent<Animator>();
-        Synchronization_title.text = "";
+        Synchronization_title.text = "シンクロ率は．．．";
         Synchronization_value.text = "";
-        Instinct_title.text = "";
-        Instinct_value.text = "";
-        _name.text = "";
+        Instinct_title.text = "あなたと○○の直感キョリは．．．";
+        Synchronization_title.color = new Color(0, 0, 0, 0);
+        Instinct_title.color = new Color(0,0,0,0);
         FadeManager.FadeIn();
         intResult = (int)(result * 100);
-        shogo_num = intResult / 10;
+        shogo_num = 10 - intResult / 10;
+        Instinct_kyori.text = Title.title[shogo_num].kyori;
+        _shogo.text ="称号：" + Title.title[shogo_num].shogo;
+        Instinct_kyori.color = new Color(1, 0, 0, 0);
+        _shogo.color = new Color(0,0,0,0);
+        unitychandx = Unitychan_running.transform.position.x - destination.x;
         destination = UnityWaitBase.transform.position - (UnityRunBase.transform.position - UnityWaitBase.transform.position) * (float)result;
+        color_dx = Color.black - Synchronization_title.color;
+
     }
-
+    private void FixedUpdate()
+    {
+        if (resultStep == ResultStep.Start)
+        {
+            StartsynTitle();
+        }
+        else if (resultStep == ResultStep.Synchronization_title)
+        {
+            fadein(2.0f,Color.black,color_dx,Synchronization_title,ResultStep.Synchronization_value);
+        }
+        else if (resultStep == ResultStep.Synchronization_value)
+        {
+            PlusValue(intResult);
+        }
+        else if (resultStep == ResultStep.Instinct_title)
+        {
+            fadein(2.0f,Color.black,color_dx,Instinct_title,ResultStep.Unitychanmove);
+        }
+        else if (resultStep == ResultStep.Unitychanmove)
+        {
+            MoveUnitychan(2.0f);
+        }
+        else if (resultStep == ResultStep.Instinct_kyori)
+        {
+            fadein(2.0f,Color.red,color_dx,Instinct_kyori,ResultStep.Shogo);
+        }
+        else if (resultStep == ResultStep.Shogo)
+        {
+            fadein(2.0f,Color.black, new Color(0, 0, 0, 1), _shogo, ResultStep.Finish);
+        }
+    }
     // Update is called once per frame
-
-
     public void Restart()
     {
         FadeManager.FadeOut(2);
     }
-    private IEnumerator PlusValue(int _intResult)
+    private void StartsynTitle()
     {
-        int Number_Display = 0;
-        while(_intResult > Number_Display)
+        SwitchStep(ResultStep.Synchronization_title);
+        Number_Display = 0;
+    }
+    private void StartInsTitle()
+    {
+        color_dx = Color.red - Instinct_title.color;
+        SwitchStep(ResultStep.Instinct_title);
+    }
+    private void PlusValue(int _intResult)
+    {
+        if(_intResult > Number_Display)
         {
             Number_Display += 1;
             Synchronization_value.text = Number_Display.ToString() + "%";
-            yield return null;
+        }
+        else
+        {
+            StartInsTitle();
         }
     }
     private IEnumerator Character_Expansion(Text _character,float expandtime = 0.1f,float expandratio = 1.2f)
@@ -73,19 +124,38 @@ public class ResultSceneManager : MonoBehaviour
         }
         _character.fontSize = originsize;
     }
-    private IEnumerator MoveUnitychan()
+    private void MoveUnitychan(float _movetime)
     {
-        runanimator.SetBool("Ruuning", true);
-        while(destination.x < Unitychan_running.transform.position.x)
+        runanimator.SetBool("Running", true);
+        if(destination.x < Unitychan_running.transform.position.x)
         {
-            Unitychan_running.transform.position -= new Vector3(0.1f,0,0);
-            yield return null;
+            Unitychan_running.transform.position -= new Vector3(unitychandx * Time.fixedDeltaTime / _movetime, 0, 0);
         }
-        runanimator.SetBool("Running", false);
+        else
+        {
+            runanimator.SetBool("Running", false);
+            StartInsKyori();
+        }
     }
-    private IEnumerator ShowKyori()
+   private  void StartInsKyori()
     {
-        yield return null;
+        color_dx = Color.red - Instinct_kyori.color;
+        SwitchStep(ResultStep.Instinct_kyori);
     }
-
+    private void fadein(float _changetime,Color color_destination,Color color_dx,Text _text,ResultStep _resultStep)
+    {;
+        if(_text.color.a < 1)
+        {
+            _text.color += color_dx * Time.fixedDeltaTime / _changetime;
+        }
+        else
+        {
+            _text.color = color_destination;
+            SwitchStep(_resultStep);
+        }
+    }
+    private void SwitchStep(ResultStep _resultStep)
+    {
+        resultStep = _resultStep;
+    }
 }
--- Assets/Kodama/Scripts/Title/Title.cs
+++ Assets/Kodama/Scripts/Title/Title.cs
@@ -7,15 +7,15 @@ public class Title : MonoBehaviour
     public static Dictionary<int, Shogo> title = new Dictionary<int, Shogo>()
     {
         {0,new Shogo("ザ・たっち","10cm") },
-        {1,new Shogo("みかみなおみ","a") },
-        {2,new Shogo("あらまきみなみ" ,"b")},
-        {3,new Shogo("友達以上恋人未満" ,"c")},
+        {1,new Shogo("身体近傍空間","20cm") },
+        {2,new Shogo("みかみなおみ" ,"b")},
+        {3,new Shogo("友達以上恋人未満" ,"1m")},
         {4,new Shogo("こだまだいき","d") },
         {5,new Shogo("はただゆうじ","e") },
         {6,new Shogo("スカイツリー","634m") },
         {7,new Shogo("東京-名古屋","352.1km") },
         {8,new Shogo("月とスッポン","384400km" )},
         {9,new Shogo("アンドロメダ","2537000光年") },
-        {10,new Shogo("こだまとあらまき" ,"パラレルワールド")}
+        {10,new Shogo("パラレルワールド" ,"∞")}
     };
 }
